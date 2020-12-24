708db806590cb9b2da49dfabf7688acf2947d0ec
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-24 13:06:00 +0530

    signin and signup page for seller and user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SellerSignin from "./screens/SellerSigninScreen";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import SellerSigninScreen from "./screens/SellerSigninScreen";
+import SellerRegisterScreen from "./screens/SellerRegisterScreen";
+import CustomerSigninScreen from "./screens/CustomerSigninScreen";
+import CustomerRegisterScreen from "./screens/CustomerRegisterScreen";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        {/* <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              LocoShop
+            </Link>
+          </div>
+          <div>
+            <Link to="/sellersignin">Seller</Link>
+          </div>
+        </header> */}
+        <main>
+          <Route path="/" component={SellerSigninScreen} exact />
+          <Route path="/sellersignin" component={SellerSigninScreen} />
+          <Route path="/sellersignup" component={SellerRegisterScreen} />
+          <Route path="/signin" component={CustomerSigninScreen} />
+          <Route path="/signup" component={CustomerRegisterScreen} />
+        </main>
+        {/* <footer className="row center">All right reserved</footer> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
