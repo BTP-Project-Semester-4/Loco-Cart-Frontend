407d07b4ae6f04783e9991d5210e89cff8cb334d
Prerit Kumar Jha <preritkrjha@gmail.com>
2020-12-24 12:21:54 +0530

    All Categories page Done

---
+++ src/components/Screens/Category/Data.js
@@ -0,0 +1,37 @@
+
+export const homeObjone = {
+   title : "Stationery",
+   description: "The stationery will offer material like paper, books, copies, writing material like pen, pencils, markers, colors.",
+   image: "https://5.imimg.com/data5/LJ/RP/MY-4878239/stationery-500x500.png"
+};
+
+export const homeObjtwo = {
+    title : "Groceries",
+   description: "A grocery is a store primarily engaged in retailing a general range of food products, which may be fresh or packaged.",
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIiQmWJAdGBHSPQJL-IdHhWYrMwUbSvIqTOQ&usqp=CAU"
+};
+
+export const homeObjthree = {
+   title : "Electronics",
+   description: " Consumer electronics or home electronics are  equipment intended for everyday use, typically in private homes.",
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgKefNtESG11OmRK1-xQhYHlO9JLtJTC5Mcg&usqp=CAU"
+};
+
+export const homeObjfour = {
+   title : "Shoes",
+   description: "A shoe is a footwear intended to protect and comfort the human foot. Also used as an item of decoration and fashion.",
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK8uPxPdCyNRld3q0v4v0AkZYge0fADARDb3otD3Uxl5jXCSu_PGKNhAoFfwa71C_3Rw9K6_Y&usqp=CAc"
+};
+
+export const homeObjfive = {
+   title : "Sports",
+   description: " The equipment ranges from balls, nets, and helmets. Can be used as protectiv help the athletes play the sport.",
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhnxvR9_DlyUe2NilNqYtkG6w6LECXWajbVg&usqp=CAU"
+};
+
+export const homeObjsix = {
+   title : "Books",
+   description: "A book is a medium for recording information in the form of writing, images, bound together and protected by a cover",
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRULdhvxYHLSDEpEV18oJadwRShXgT2cRorPA&usqp=CAU"
+};
+  
