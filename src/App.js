4374b79a5a2b5e1fd75309777787a6c43292e540
Prerit Kumar Jha <preritkrjha@gmail.com>
2020-12-24 16:36:17 +0530

    Subcategory Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Categories } from './components/Screens/Category/Category';
+import { SubCategories } from './components/Screens/Category/SubCategory/SubCategories';
  
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/all_categories" exact component={Categories}/>
+        <Route path="/Category" exact component={Categories}/>
+        <Route path="/Category/:id" component={SubCategories}/>     
       </Switch>
     </Router>
   );
