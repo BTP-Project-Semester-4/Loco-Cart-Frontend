57bec0c68948468df1b857027c3ef2d3de029dca
vijayjoshi16 <joshivijay324@gmail.com>
2020-12-27 14:49:36 +0530

    Sync to remote repo

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ import { Categories } from './components/Screens/Category/Category';
  
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
     </div>
-=======
-    <Router>
-      <Switch>
-        <Route path="/all_categories" exact component={Categories}/>
-      </Switch>
-    </Router>
->>>>>>> 1d5b559278e1f232086b2793a1ba1410d28c78f7
   );
 }
 
