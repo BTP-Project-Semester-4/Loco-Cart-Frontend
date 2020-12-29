a11aa6a0b39e1a2996c90109f17667d532563f31
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-29 15:45:18 +0530

    other details page added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { Categories } from "./components/Screens/Category/Category";
 import { SubCategories } from "./components/Screens/Category/SubCategory/SubCategories";
 import SellerProfile from "./components/Screens/SellerProfile/SellerProfile";
 import UserProfile from "./components/Screens/UserProfile/UserProfile";
-import CustomerOTP from './components/Screens/CustomerOTP/CustomerOTP';
-import SellerOTP from './components/Screens/SellerOTP/SellerOTP';
+import CustomerOTP from "./components/Screens/CustomerOTP/CustomerOTP";
+import SellerOTP from "./components/Screens/SellerOTP/SellerOTP";
 import NotFound from "./components/NotFound/NotFound";
+import otherSellerDetails from "./components/Screens/SellerSignUp/otherSellerDetails/otherSellerDetails";
 function App() {
   return (
     <BrowserRouter>
@@ -20,12 +21,16 @@ function App() {
           <Route path="/sellersignin" component={SellerSigninScreen} />
           <Route path="/sellersignup" component={SellerRegisterScreen} />
           <Route path="/signin" component={CustomerSigninScreen} />
-          <Route path="/signup" component={CustomerRegisterScreen} />
+          <Route path="/signup" component={CustomerRegisterScreen} exact />
+          <Route
+            path="/signup/othersellerdetails"
+            component={otherSellerDetails}
+          />
           <Route path="/category" exact component={Categories} />
           <Route path="/category/:id" component={SubCategories} />
           <Route path="/sellerprofile" component={SellerProfile} />
           <Route path="/userprofile" component={UserProfile} />
-          <Route path='/customerotp' component={CustomerOTP} />
+          <Route path="/customerotp" component={CustomerOTP} />
           <Route path="/sellerotp" component={SellerOTP} />
           <Route component={NotFound} />
         </Switch>
