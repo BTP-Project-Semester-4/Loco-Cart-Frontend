e2b237091501ed432852545beca9ef0e1292efbf
vijayjoshi16 <joshivijay324@gmail.com>
2021-01-06 12:39:28 +0530

    Rebuilt user profile and seller profile using material-ui

--- src/components/Screens/SellerProfile/SellerProfile.js
+++ src/components/Screens/SellerProfile/SellerProfile.js
@@ -4,75 +4,140 @@ import EmailIcon from '@material-ui/icons/Email';
 import PhoneIcon from '@material-ui/icons/Phone';
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import StarIcon from '@material-ui/icons/Star';
+import Grid from '@material-ui/core/Grid';
 
 const SellerProfile = (props)=>{
     return(
-        <div className="container">
-            <div className="row intro">
-                <div className="col s12 m6 l4">
-                    <img className="userImage" src="https://pbs.twimg.com/profile_images/1284557829651656705/P9L-PaCf_400x400.jpg"/>
-                </div>
-                <div className="col s12 m6 l8 userDetail">
+        <Grid container className="all_content">
+            <Grid item xs={12} sm={12} md={12} lg={12} className="intro">
+            <Grid container>
+                <Grid item xs={12} sm={12} md={4} lg={4}>
+                <img className="userImage" src="https://pbs.twimg.com/profile_images/1284557829651656705/P9L-PaCf_400x400.jpg"/>
+                </Grid>
+                <Grid item xs={12} sm={12} md={8} lg={8} className="userDetail">
                     <h1 className="name">KACHRA SETH</h1>
                     <h3 className="location">Pune, India</h3>
                     <p className="interest">Category: Fashion & Clothing, Electronics, Books</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col s12 m3 l3 extra-data">
-                    <h6 className="section-heading">Other Details</h6>
-                    <div className="allExtra" >
-                    <EmailIcon color="primary"/> <span className="extra-info">email@email.com</span>
-                    <br></br>
-                    <PhoneIcon color="primary"/><span className="extra-info">+91 9999999999</span>
-                    <br></br>
-                    <AssignmentTurnedInIcon color="primary"/><span className="extra-info">12 Orders till now</span>
-                    <br></br>
-                    <StarIcon color="primary"/><span className="extra-info">4.3 average rating</span>
-                    </div>
-                </div>
-                <h6 className="section-heading">Products</h6>
-                <div className="products">
-                    <div className="col s12 m4 l4 product">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvHxnUgAMTCsiH_RIXW8D6gRcYIqB9dY_jSQ&usqp=CAU"
-                            className="product-img"
-                        />
-                        <h3 className="product-name">Shyam ka coat</h3>
-                        <p className="product-price">₹3000</p>
-                    </div>
-                    <div className="col s12 m4 l4 product">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2li0nH-jB5wmghNuwg9bsjMhpFlcFEEfLDA&usqp=CAU"
-                            className="product-img"
-                        />
-                        <h3 className="product-name">Raju ke joote</h3>
-                        <p className="product-price">₹150</p>
-                    </div>
-                    <div className="col s12 m3 l3"></div>
-                    <div className="col s12 m4 l4 product">
-                        <img src="https://static.fratello.com/2017/06/DSC_6425-2.jpg"
-                            className="product-img"
-                        />
-                        <h3 className="product-name">Gold watch</h3>
-                        <p className="product-price">₹15000</p>
-                    </div>
-                    <div className="col s12 m4 l4 product">
-                        <img src="https://static.toiimg.com/photo/msid-71362507/71362507.jpg"
-                            className="product-img"
-                        />
-                        <h3 className="product-name">Jersey</h3>
-                        <p className="product-price">₹1200</p>
-                    </div>
-                    <div className="col s12 m3 l3"></div>
-                    <div className="col s12 m4 l4 product">
-                        <img src="https://cdn.shopify.com/s/files/1/1089/8530/products/TB0905-NB_large.jpg?v=1573578354"
-                            className="product-img"
-                        />
-                        <h3 className="product-name">Bag</h3>
-                        <p className="product-price">₹40 lakh</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+                </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <h6 className="section-heading">Other Details</h6>
+                        <div className="allExtra" >
+                        <EmailIcon color="primary"/> <span className="extra-info">email@email.com</span>
+                         <br></br>
+                         <PhoneIcon color="primary"/><span className="extra-info">+91 9999999999</span>
+                         <br></br>
+                         <AssignmentTurnedInIcon color="primary"/><span className="extra-info">12 Orders till now</span>
+                         <br></br>
+                         <StarIcon color="primary"/><span className="extra-info">4.3 average rating</span>
+                         </div>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={8} lg={8} className="products">
+                        <h6 className="section-heading">Products</h6>
+                        <Grid container xs={12} sm={12} md={12} lg={12}>
+                        <Grid item xs={12} sm={5} md={5} lg={5} className="product">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvHxnUgAMTCsiH_RIXW8D6gRcYIqB9dY_jSQ&usqp=CAU"
+                                className="product-img"
+                            />
+                            <h3 className="product-name">Shyam ka coat</h3>
+                            <p className="product-price">₹3000</p>
+                        </Grid>
+                        <Grid item xs={12} sm={5} md={5} lg={5} className="product">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2li0nH-jB5wmghNuwg9bsjMhpFlcFEEfLDA&usqp=CAU"
+                                className="product-img"
+                            />
+                            <h3 className="product-name">Raju ke joote</h3>
+                            <p className="product-price">₹150</p>
+                        </Grid>
+                        <Grid item xs={12} sm={5} md={5} lg={5} className="product">
+                            <img src="https://static.fratello.com/2017/06/DSC_6425-2.jpg"
+                                className="product-img"
+                            />
+                            <h3 className="product-name">Gold watch</h3>
+                            <p className="product-price">₹15000</p>
+                        </Grid>
+                        <Grid item xs={12} sm={5} md={5} lg={5} className="product">
+                            <img src="https://cdn.shopify.com/s/files/1/1089/8530/products/TB0905-NB_large.jpg?v=1573578354"
+                                className="product-img"
+                            />
+                            <h3 className="product-name">Bag</h3>
+                            <p className="product-price">₹40 lakh</p>
+                        </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+
+        // <div className="container">
+        //     <div className="row intro">
+        //         <div className="col s12 m6 l4">
+        //             <img className="userImage" src="https://pbs.twimg.com/profile_images/1284557829651656705/P9L-PaCf_400x400.jpg"/>
+        //         </div>
+        //         <div className="col s12 m6 l8 userDetail">
+        //             <h1 className="name">KACHRA SETH</h1>
+        //             <h3 className="location">Pune, India</h3>
+        //             <p className="interest">Category: Fashion & Clothing, Electronics, Books</p>
+        //         </div>
+        //     </div>
+        //     <div className="row">
+        //         <div className="col s12 m3 l3 extra-data">
+        //             <h6 className="section-heading">Other Details</h6>
+        //             <div className="allExtra" >
+        //             <EmailIcon color="primary"/> <span className="extra-info">email@email.com</span>
+        //             <br></br>
+        //             <PhoneIcon color="primary"/><span className="extra-info">+91 9999999999</span>
+        //             <br></br>
+        //             <AssignmentTurnedInIcon color="primary"/><span className="extra-info">12 Orders till now</span>
+        //             <br></br>
+        //             <StarIcon color="primary"/><span className="extra-info">4.3 average rating</span>
+        //             </div>
+        //         </div>
+        //         <h6 className="section-heading">Products</h6>
+        //         <div className="products">
+        //             <div className="col s12 m4 l4 product">
+        //                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvHxnUgAMTCsiH_RIXW8D6gRcYIqB9dY_jSQ&usqp=CAU"
+        //                     className="product-img"
+        //                 />
+        //                 <h3 className="product-name">Shyam ka coat</h3>
+        //                 <p className="product-price">₹3000</p>
+        //             </div>
+        //             <div className="col s12 m4 l4 product">
+        //                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2li0nH-jB5wmghNuwg9bsjMhpFlcFEEfLDA&usqp=CAU"
+        //                     className="product-img"
+        //                 />
+        //                 <h3 className="product-name">Raju ke joote</h3>
+        //                 <p className="product-price">₹150</p>
+        //             </div>
+        //             <div className="col s12 m3 l3"></div>
+        //             <div className="col s12 m4 l4 product">
+        //                 <img src="https://static.fratello.com/2017/06/DSC_6425-2.jpg"
+        //                     className="product-img"
+        //                 />
+        //                 <h3 className="product-name">Gold watch</h3>
+        //                 <p className="product-price">₹15000</p>
+        //             </div>
+        //             <div className="col s12 m4 l4 product">
+        //                 <img src="https://static.toiimg.com/photo/msid-71362507/71362507.jpg"
+        //                     className="product-img"
+        //                 />
+        //                 <h3 className="product-name">Jersey</h3>
+        //                 <p className="product-price">₹1200</p>
+        //             </div>
+        //             <div className="col s12 m3 l3"></div>
+        //             <div className="col s12 m4 l4 product">
+        //                 <img src="https://cdn.shopify.com/s/files/1/1089/8530/products/TB0905-NB_large.jpg?v=1573578354"
+        //                     className="product-img"
+        //                 />
+        //                 <h3 className="product-name">Bag</h3>
+        //                 <p className="product-price">₹40 lakh</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
--- src/components/Screens/UserProfile/UserProfile.js
+++ src/components/Screens/UserProfile/UserProfile.js
@@ -4,98 +4,95 @@ import EmailIcon from '@material-ui/icons/Email';
 import PhoneIcon from '@material-ui/icons/Phone';
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import StarIcon from '@material-ui/icons/Star';
+import Grid from '@material-ui/core/Grid';
 
 const UserProfile = (props)=>{
     return(
-        <div className="container">
-            <div className="row intro">
-                <div className="col s12 m6 l4">
-                    <img className="userImage" src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg"/>
-                </div>
-                <div className="col s12 m6 l8 userDetail">
+        <Grid container className="all_content">
+            <Grid item xs={12} sm={12} md={12} lg={12} className="intro">
+            <Grid container>
+                <Grid item xs={12} sm={12} md={4} lg={4}>
+                <img className="userImage" src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg"/>
+                </Grid>
+                <Grid item xs={12} sm={12} md={8} lg={8} className="userDetail">
                     <h1 className="name">ELON MUSK</h1>
                     <h3 className="location">Pune, India</h3>
                     <p className="interest">Interests: Fashion & Clothing, Electronics, Books</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col s12 m4 l4 extra-data">
-                    <h6 className="section-heading">Other Details</h6>
-                    <div className="allExtra" >
-                    <EmailIcon color="primary"/> <span className="extra-info">email@email.com</span>
-                    <br></br>
-                    <PhoneIcon color="primary"/><span className="extra-info">+91 9999999999</span>
-                    <br></br>
-                    <AssignmentTurnedInIcon color="primary"/><span className="extra-info">12 Orders till now</span>
-                    <br></br>
-                    <StarIcon color="primary"/><span className="extra-info">4.3 average rating</span>
-                    </div>
-                </div>
-                    <h6 className="section-heading">Reviews</h6>
-                    <div className="reviews">
-                    <div className="col s12 m8 l8 review">
-                        <div className="col s12 m4 l4">
+                </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <h6 className="section-heading">Other Details</h6>
+                        <div className="allExtra" >
+                        <EmailIcon color="primary"/> <span className="extra-info">email@email.com</span>
+                         <br></br>
+                         <PhoneIcon color="primary"/><span className="extra-info">+91 9999999999</span>
+                         <br></br>
+                         <AssignmentTurnedInIcon color="primary"/><span className="extra-info">12 Orders till now</span>
+                         <br></br>
+                         <StarIcon color="primary"/><span className="extra-info">4.3 average rating</span>
+                         </div>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={8} lg={8} className="all-reviews">
+                        <h6 className="section-heading">Reviews</h6>
+                        <Grid container xs={12} sm={12} md={12} lg={12} className="review">
+                        <Grid item xs={12} sm={12} md={4} lg={4}>
                             <img src="https://avatars3.githubusercontent.com/u/54065357?s=400&u=e02a7adcdfffa13bf8ca5a935793139752b1a4ff&v=4"
-                                className="seller-img"
-                            />
+                            className="seller-img"/>
                             <h3 className="seller-name">Nalin Sellers</h3>
                             <p className="seller-rating">Rated 5</p><StarIcon color="primary"/>
-                        </div>
-                        <div className="col s12 m4 l4 review-text" style={{width:"inherit", textAlign:"left"}}>
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={8} lg={8} >
+                            <p className="review-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                             </p>
-                        </div>
-                    </div>
-                    <div className="col s12 m4 l4"></div>
-                    <div className="col s12 m8 l8 review">
-                        <div className="col s12 m4 l4">
+                        </Grid>
+                        </Grid>
+                        <Grid container xs={12} sm={12} md={12} lg={12} className="review">
+                        <Grid item xs={12} sm={12} md={4} lg={4}>
                             <img src="https://avatars0.githubusercontent.com/u/67575900?s=400&u=a87b16f58b6cf169801a1f7c97237b039dc2bf76&v=4"
-                            className="seller-img"
-                            />
+                            className="seller-img"/>
                             <h3 className="seller-name">Prerit Retailers</h3>
                             <p className="seller-rating">Rated 5</p><StarIcon color="primary"/>
-                        </div>
-                        <div className="col s12 m4 l4 review-text" style={{width:"inherit", textAlign:"left"}}>
-                            <p>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={8} lg={8} >
+                            <p className="review-text">
                             Sed ut perspiciatis unde omnis velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                             </p>
-                        </div>
-                    </div>
-                    <div className="col s12 m4 l4"></div>
-                    <div className="col s12 m8 l8 review">
-                        <div className="col s12 m4 l4">
+                        </Grid>
+                        </Grid>
+                        <Grid container xs={12} sm={12} md={12} lg={12} className="review">
+                        <Grid item xs={12} sm={12} md={4} lg={4}>
                             <img src="https://virtual-interview-platform.herokuapp.com/photos/AMIT.jpeg"
-                                className="seller-img"
-                            />
-                            <h3 className="seller-name">Amit Collections</h3>
+                            className="seller-img"/>
+                            <h3 className="seller-name">Amit Retailers</h3>
                             <p className="seller-rating">Rated 5</p><StarIcon color="primary"/>
-                        </div>
-                        <div className="col s12 m4 l4 review-text" style={{width:"inherit", textAlign:"left"}}>
-                            <p>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={8} lg={8} >
+                            <p className="review-text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                             </p>
-                        </div>
-                    </div>
-                    <div className="col s12 m4 l4"></div>
-                    <div className="col s12 m8 l8 review">
-                        <div className="col s12 m4 l4">
+                        </Grid>
+                        </Grid>
+                        <Grid container xs={12} sm={12} md={12} lg={12} className="review">
+                        <Grid item xs={12} sm={12} md={4} lg={4}>
                             <img src="https://avatars1.githubusercontent.com/u/54314949?s=400&u=038707b14efd687499a9b7864547f5386bbb3bb4&v=4"
-                                className="seller-img"
-                            />
-                            <h3 className="seller-name">Vijay Digital Mart</h3>
+                            className="seller-img"/>
+                            <h3 className="seller-name">Vijay Mart</h3>
                             <p className="seller-rating">Rated 5</p><StarIcon color="primary"/>
-                        </div>
-                        <div className="col s12 m4 l4 review-text" style={{width:"inherit", textAlign:"left"}}>
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={8} lg={8} >
+                            <p className="review-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                             </p>
-                        </div>
-                    </div>
-                    </div>
-                    
-            </div>
-        </div>
+                        </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
