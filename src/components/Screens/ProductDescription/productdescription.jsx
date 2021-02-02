e0d0034261ed18a8324bbd3b6cbb887511070668
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2021-02-02 10:22:05 +0530

    Update productdescription.jsx

--- src/components/Screens/ProductDescription/productdescription.jsx
+++ src/components/Screens/ProductDescription/productdescription.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./productdescription.css";
-// import p2 from "./p3.png";
+import p2 from "./p3.png";
 import { Link } from "react-router-dom";
 
 const Productdesc = () => {
   return (
     <>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {/* <title>amazon | Product Page</title> */}
-      <link href="productdescription.css" rel="stylesheet" />
-      <header>{/* replace this header with yours....... */}</header>
-      <section id="product-info">
-        <div className="item-image-parent">
-          <div className="item-list-vertical">
-            {/* <div className="thumb-box">
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  {/* <title>amazon | Product Page</title> */}
+  <link href="productdescription.css" rel="stylesheet" />
+  <header>
+    {/* replace this header with yours....... */}
+  </header>
+  <section id="product-info">
+  
+    <div className="item-image-parent" >
+      <div className="item-list-vertical">
+        {/* <div className="thumb-box">
           <img src="https://i.ibb.co/VJf6fXm/thumb1.jpg" alt="thumbnail" />
         </div> */}
-            {/* <div className="thumb-box">
+        {/* <div className="thumb-box">
           <img src="https://i.ibb.co/Jt5zc58/thumb2.jpg" alt="thumbnail" />
         </div>
         <div className="thumb-box">
@@ -27,36 +30,32 @@ const Productdesc = () => {
         <div className="thumb-box">
           <img src="https://i.ibb.co/60hPGy2/thumb4.jpg" alt="thumbnail" />
         </div> */}
-          </div>
-          <div className="item-image-main">
-            <img src={""} alt="default" />
-          </div>
-        </div>
-        <div className="item-info-parent">
-          {/* main info */}
-          <div className="main-info">
-            <h3>Apple iPhone X </h3>
-            {/* <div className="star-rating">
+      </div>
+      <div className="item-image-main">
+        <img src={p2} alt="default" />
+      </div>
+    </div>
+    <div className="item-info-parent">
+      {/* main info */}
+      <div className="main-info">
+        <h3>APPLE IPHONE X </h3>
+        {/* <div className="star-rating">
           <span>★★★★</span>★            
         </div> */}
-            <h3>
-              <p>
-                PRICE:<span id="price">₹ 449.00</span>
-              </p>
-            </h3>
-          </div>
-          {/* Choose */}
-          <div className="select-items">
-            <div className="change-color">
-              {/* <label><b>Colour:</b> Black</label><br /> */}
-              {/* <div className="thumb-box">
+       <h3><p><span id="price">₹ 449.00</span></p></h3>
+      </div>
+      {/* Choose */}
+      <div className="select-items">
+        <div className="change-color">
+          {/* <label><b>Colour:</b> Black</label><br /> */}
+          {/* <div className="thumb-box">
             <img src={p2} alt="thumbnail" />
           </div> */}
-              {/* <div className="thumb-box">
+          {/* <div className="thumb-box">
             <img src="https://i.ibb.co/C297yD0/select2.jpg" alt="thumbnail" />
           </div> */}
-            </div>
-            {/* <div className="change-size">
+        </div>
+        {/* <div className="change-size">
           <label><b>Size:</b></label><br />
           <select>
             <option>S</option>
@@ -66,30 +65,32 @@ const Productdesc = () => {
             <option>2XL</option>
           </select>
         </div> */}
-            <div className="description">
-              <ul>
-                <li>Description:14.73 cm Super Retina Screen</li>
-                <li>Display: Innovative Display Technology</li>
-                <li>Model Number: MQA62HN/A</li>
-                <li>Model Name:iPhone X</li>
-                <li>Browse Type:Smartphones</li>
+        <div className="description">
+          <ul>
+            <li><u style={{fontWeight:'bold',fontFamily:'Trebuchet MS',fontSize:'23px'}}>Description:</u>14.73 cm Super Retina Screen</li>
+            <li><u style={{fontWeight:'bold',fontFamily:'Trebuchet MS',fontSize:'23px'}}>Display:</u>Innovative Display Technology</li>
+            <li><u style={{fontWeight:'bold',fontFamily:'Trebuchet MS',fontSize:'23px'}}>Model Number:</u>MQA62HN</li>
+            <li><u style={{fontWeight:'bold',fontFamily:'Trebuchet MS',fontSize:'23px'}}>Model Name:</u>iPhone X</li>
+            <li><u style={{fontWeight:'bold',fontFamily:'Trebuchet MS',fontSize:'23px'}}>Browse Type:</u>Smartphones</li>
+            
+            <li>
 
-                <li>
-                  <Link to="/cart">
-                    <button class="button button2">ADD TO CART</button>
-                  </Link>
-
-                  <Link to="/sellerprofile">
-                    <button class="button button1">SELLER PROFILE</button>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* Description */}
+            <Link to='/cart'>
+            <button class="button button2">ADD TO CART</button>
+            </Link>
+            
+            <Link to='/sellerprofile'>
+            <button class="button button1">SELLER PROFILE</button>
+            </Link>
+            </li>
+           
+          </ul>
         </div>
-      </section>
-    </>
+      </div>
+      {/* Description */}
+    </div>
+  </section>
+</>
   );
 };
 export default Productdesc;
