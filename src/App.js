6a2aa867c0c105b92433e60f1e1ae8ccd7ca7de2
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-03-07 22:31:00 +0530

    edit profile

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import Notifications from "./components/Screens/Notification/Bes.jsx";
 import BiddingScreenSeller from "./components/Screens/BiddingScreenSeller/BiddingScreenSeller";
 import AllSellers from "./components/Screens/AllSellers/AllSellers";
 import SellerProducts from "./components/Screens/SellerProducts/SellerProducts";
+import EditProfile from "./components/Screens/EditProfile/EditProfile";
 function App() {
   return (
     <BrowserRouter>
@@ -60,6 +61,7 @@ function App() {
             <Route path="/BiddingSeller" component={BiddingScreenSeller} />
             <Route path="/allsellers/:id" component={AllSellers} />
             <Route path="/sellerproducts" component={SellerProducts} />
+            <Route path="/editprofile" component={EditProfile}/>
             <Route component={NotFound} />
           </Switch>
         </main>
