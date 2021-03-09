cdc5eb1d850396043d9e10c1180278fc369bd12f
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2021-03-09 20:44:31 +0530

    Update SellerSigninScreen.js

--- src/components/Screens/SellerSignIn/SellerSigninScreen.js
+++ src/components/Screens/SellerSignIn/SellerSigninScreen.js
@@ -170,7 +170,7 @@ export default function SellerSigninScreen() {
             <Box mt={3} />
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+               <Link href="forgotpassword" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
