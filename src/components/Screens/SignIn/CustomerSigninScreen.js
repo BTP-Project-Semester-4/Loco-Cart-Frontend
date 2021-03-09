fe48def14d4e340e0644151127aac9b743d14463
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2021-03-09 20:44:47 +0530

    Update CustomerSigninScreen.js

--- src/components/Screens/SignIn/CustomerSigninScreen.js
+++ src/components/Screens/SignIn/CustomerSigninScreen.js
@@ -174,7 +174,7 @@ export default function CustomerSigninScreen() {
             <Box mt={3} />
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="forgotpassword" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
