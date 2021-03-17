9044268d117a3d895f0b79597f41f5cbead78057
vijayjoshi16 <joshivijay324@gmail.com>
2021-03-17 10:28:00 +0530

    Minor changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -587,7 +587,7 @@ export default function Header() {
                 />
               </ListItem>
             </Link>
-            <Link to="/biddingSeller" onClick={handleDrawerClose}>
+            <Link to="/allactivebids" onClick={handleDrawerClose}>
               <ListItem button key="Bid Products">
                 <ListItemIcon>
                   <GavelIcon />
